a620d8efc47a1bb8c276eda13b8ed49622c6be0f
Jwoo0725 <101973624+Jwoo0725@users.noreply.github.com>
2024-04-03 13:32:44 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
 # import Manager
 # import Student 
 import numpy as np
+import Subject
+import managerCheck
+import add
+
 
 UserList = {} # 이중 dictionary형태 UserList = { ID : { Type : PW } }
+
+
+def addClasses(ID) :
+    print("------수강신청-----")
+    return 0
 
 def managerCheck(CheckNum):
     managerCheckNum = '1234'
     if bool(CheckNum == managerCheckNum) : return CheckNum
     else : return print("\nWrong Number.")
 
-def addClasses(ID) :
-    print("------수강신청-----")
-    return 0
+def getSubject():
+    subjectFile = open("/Users/jwoo/Documents/Python/ImbededSystem/Practice1/basics.txt", 'r')
+    subject = subjectFile.readlines()
+    subjectList = [x.split() for x in subject]
+    subjectInfo = {}
+    for i in subjectList: print(*i)
+    # print(subjectInfo )
+    # for i in subjectList:
+    #     subjectInfo = {'과목번호':subjectList[i][0], '과목명':subjectList[i][1], '전필여부':subjectList[i][2], '학점':subjectList[i][3],'수업시간':subjectList[i][4], '장소':subjectList[i][5] }
+    # print(subjectInfo)
+getSubject()
+
+# def addSubject():
+       
+
 
 print("\n-------------------------\n     Ajou University     \n-------------------------") 
 
@@ -50,7 +71,6 @@ while(1):
                     else : break 
                 Password = input("Enter your new Password : ")
                 UserList[ID] = {Password : Type}
-                print(UserList)
                 #### + def(Type구분) : Type구분해서 사용자 structure 생성해주는 함수
                 print("\n*** Welcome to Ajou University! ****\n*** We return to the first Page. ***\n")
                 break # 첫페이지로 이동
@@ -79,22 +99,22 @@ while(1):
             else : pass
 
             print("Login success.\n")
-            subjectFile = open('/Users/jwoo/Documents/Python/ImbededSystem/Practice1/basics.txt', mode = 'r' )
-            print(subjectFile.read())
-            subjectFile.close()
+            getSubject()
 
             if UserList[ID][Password] == "Manager" :
                 while 1:
                     print("\nChoose the Option.")
                     print("1. Add subject")
                     print("2. Delete subject")
-                    print("3. Exit")
+                    print("3. Modify subject")
+                    print("4. Exit")
                     managerOption = input("Enter the number : ")
                     # match managerOption :
                     #     case '1':
-
+                            
                     #     case '2':
                     #     case '3':
+                    #     case '4':
                     #     case _:
                     #         print("\n3-This option is not available. Please enter the available number.")
                     #         continue
@@ -107,4 +127,3 @@ while(1):
         case _: #Wrong Choice
              print("\n1-This option is not available. Please enter the available number.")
              continue
- 
