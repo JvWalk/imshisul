99c5b559a66fa0b57751899cf727dfd03298cf52
jisoon <chamkae11@ajou.ac.kr>
2024-04-05 03:01:12 +0900

    과목을 보여주는 GUI 생성

---
+++ GwamokShower.py
@@ -0,0 +1,29 @@
+import tkinter as tk
+##from tkinter.tix import *
+import GwamokCaller
+
+popup=tk.Tk()
+popup.title("YUN DAE HEE")
+popup.geometry("900x1000+100+1")
+popup.resizable(False, False)
+
+
+label = tk.Label(popup, height=1)
+show = tk.Label(text="class number", width=20, height=2)
+show.grid(row=0, column=0)
+show = tk.Label(text="class name", width=20, height=2)
+show.grid(row=0, column=1)
+show = tk.Label(text="class pilsu", width=20, height=2)
+show.grid(row=0, column=2)
+show = tk.Label(text="class credit", width=20, height=2)
+show.grid(row=0, column=3)
+show = tk.Label(text="class time", width=20, height=2)
+show.grid(row=0, column=4)
+show = tk.Label(text="class room", width=20, height=2)
+show.grid(row=0, column=5)
+for i in range(GwamokCaller.gwamok.shape[0]):
+    for j in range(GwamokCaller.gwamok.shape[1]):
+        show  = tk.Label(text=GwamokCaller.gwamok[i][j], width=20, height=2)
+        show.grid(row=i+1, column=j)
+    
+popup.mainloop()
