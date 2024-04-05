a1875263ed56fadc7ea660078833c20b58af329b
Jwoo0725 <101973624+Jwoo0725@users.noreply.github.com>
2024-04-05 19:43:10 +0900

    Update AddSub.py

--- AddSub.py
+++ AddSub.py
@@ -11,18 +11,19 @@ time2=int
 grade=int
 roomnum=int
 def classno():
-        i = input("enter class number:")
+        i = input("Enter class number : ")
         Newsub.insert(0, i)
 def name():
-        i = input("enter class name:")
+        i = input("Enter class name : ")
         Newsub.insert(1, i)
 def pilsu():
         while 1 :
-                i = input("is it major pilsu? answer y for 'yes', n for 'no'")
-                if i == 'y' :
+                # i = input("Is it major pilsu? answer y for 'yes', n for 'no'")
+                i = input("\nChoose the class type\n1.Required\n2.Elective\nEnter the number : ")
+                if i == '1' :
                         Newsub.insert(2, 'Required')
                         break
-                elif i == 'n' :
+                elif i == '2' :
                         Newsub.insert(2, 'Elective')
                         break
                 else : 
@@ -30,27 +31,47 @@ def pilsu():
                         continue
 
 def hakjum():
-        i = input("enter class credit:")
+        i = input("Enter class credit:")
         Newsub.insert(3, i)
 
 def time():
-        day1 = input("enter day1 :")
-        time1 = input("enter time1 :")
-        day2 = input("enter day2 :")
-        time2 = input("enter time2 :")
-        Newsub.insert(4, day1+'#'+time1+'#'+day2+'#'+time2)
+        d1 = input("\nChoose the day of the first class.\n1. Mon    2. Tue    3. Wed    4. Thu    5. Fri\nEnter the number : ")
+        t1 = input("\nChoose the time of the class.\n1. A | 9:00\n2. B | 10:00\n3. C | 11:00\n4. D | 12:00\n5. E | 13:00\n6. F | 14:00\nEnter the number : ")
+        day1, time1 = tradeTime(d1, t1)
+        hour1 = input("\nChoose the hour class will take place.\n1. Theory class : 1 hour \n2. Experimental class : 4 hour \nEnter the number : ")
+        d2 = input("\nChoose the day of the second class.\n1. Mon    2. Tue    3. Wed    4. Thu    5. Fri\nEnter the number : ")
+        t2 = input("\nChoose the time of the class.\n1. A | 9:00\n2. B | 10:00\n3. C | 11:00\n4. D | 12:00\n5. E | 13:00\n6. F | 14:00\nEnter the number : ")
+        day2, time2 = tradeTime(d2, t2)
+        hour2 = input("\nChoose the hour class will take place.\n1. Theory class : 1 hour \n2. Experimental class : 4 hour \nEnter the number : ")
+        Newsub.insert(4, day1+'#'+time1+hour1+'#'+day2+'#'+time2+hour2)
 
 def room():
-        gwan = input("enter hall :")
-        roomnum = input("enter room number :")
+        gwan = input("Enter hall : ")
+        roomnum = input("Enter room number : ")
 		#if 시간과 강의실 중복검사
         Newsub.insert(5, gwan+'#'+roomnum)
         
 def add():
-	f = open("basics.txt",'a')
-	for i in range(6) :
-                arrvar = Newsub[i]
-                f.write(arrvar)
+        f = open("basics.txt",'a')
+        f.write('\n')
+        for i in range(6) :
+                idk = Newsub[i]
+                f.write(idk)
                 f.write('\t')
-	f.close()
+        f.close()
 
+def tradeTime(day, time):
+        match day:
+                case '1': day = 'Mon'
+                case '2': day = 'Tue'
+                case '3': day = 'Wed'
+                case '4': day = 'Thu'
+                case '5': day = 'Fri'
+        match time:
+                case '1': time = 'A'
+                case '2': time = 'B'
+                case '3': time = 'C'
+                case '4': time = 'D'
+                case '5': time = 'E'
+                case '6': time = 'F'
+        return day, time
