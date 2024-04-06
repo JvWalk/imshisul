3c6706ba3ce3371dc1dd22f92af729d145d7b3ac
jisoon <chamkae11@ajou.ac.kr>
2024-04-07 03:42:30 +0900

    student class 완성본

--- StudentCaller.py
+++ StudentCaller.py
@@ -1,22 +1,46 @@
 import numpy as np
+import Student
+import time
+import datetime
+
+chamkae11 = Student.student('chamkae11')  #아이디 예시(ID = chamkae11)
+
+Timeleft = 300          #사용할수 있는 시간 = 300초
 
 while (1) : 
-print('you are student class. choose what action you will make')
-print('type 1 to add class, type 2 to delete class, type 3 to see what class you\'ve added')
-	action = input('')
-	match action : 
-		case '1':
-			print('What class are you trying to add?')
-			print('(If you didn\'t scheduled 4 or more required class, we notice you that your call can be rejected)')
-			classnum = input('')
-			aa = Addclass(classnum)
-			if aa is 1 : 
-				break
-		case '2' :
-			DeleteClass()
-		case '3' :
-			SeeSugang()
-		case _: 
-			print('wrong input! please try again...')
+    Starttime = time.time()  #시작 시간
+    if Timeleft < 0 :   #남은 시간이 없을 시
+        print('Time out! please log in again.\n')
+        break               #로그인 화면으로 돌아감
 
-#time thing we have to consider
+    print('You are student class. choose what action you will make')
+    print('Type 1 to add class, type 2 to delete class, type 3 to see what class you\'ve added:') #student 수강신청 화면
+    print('(Time left untill time out : ',round(Timeleft, 1),' sec)')
+    action = input('')
+    
+    match action :              #input 확인
+        case '1':               #수강신청 
+            print('What class are you adding?')
+            print('(Type a to abort)')
+
+            cl_num = input('')  #과목 번호 확인
+            if cl_num != 'a' :
+                    Student.student.Addclass(chamkae11,cl_num)          #수강신청 함수 호출
+            Timeleft = Timeleft + Starttime- time.time()        #걸린 시간 저장
+            
+        case '2' :              #수강취소
+            print('What class are you deleting?')
+            print('(Type a to abort :)')
+            cl_num = input('')  #과목 번호 확인
+            if cl_num != 'a' :
+                    Student.student.Deleteclass(chamkae11,cl_num)  #수강정정 함수 호출
+            Timeleft = Timeleft + Starttime- time.time()        #걸린 시간 저장
+            
+        case '3' :
+            schedule = Student.student.Showschedule(chamkae11)  #스케줄표 열어주는 함수 호출
+            Timeleft = Timeleft + Starttime- time.time()        #스케줄표를 닫을 때까지 걸린 시간 저장
+            print('\n')
+        case _: 
+            print('Wrong input! please try again...\n')         #잘못된 입력시 에러
+            Timeleft = Timeleft + Starttime- time.time()
+
